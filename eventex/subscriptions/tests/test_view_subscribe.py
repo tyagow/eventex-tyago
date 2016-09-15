967dac30bb7575e18519acd23eb26c1b97901789
Tiago Almeida <tyagow@hotmail.com.br>
2016-09-15 12:05:59 -0300

    Thank you page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,84 +1,76 @@
 from django.core import mail
 from django.test import TestCase
+
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
 
 class SubscribeGet(TestCase):
-	def setUp(self):
-		self.resp = self.client.get('/inscricao/')
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
 
-	def test_get(self):
-		"""Get /inscricao/ must return code 200"""
-		self.assertEqual(200, self.resp.status_code)
+    def test_get(self):
+        """Get /inscricao/ must return code 200"""
+        self.assertEqual(200, self.resp.status_code)
 
-	def test_template(self):
-		"""Must use subscriptions/subscription_form.html"""
-		self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
+    def test_template(self):
+        """Must use subscriptions/subscription_form.html"""
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
 
-	def test_html(self):
-		"""Html must contain input tags"""
-		tags = (('<form', 1),
-		        ('<input', 6),
-		        ('type="text"', 3),
-		        ('type="email"', 1),
-		        ('type="submit"', 1))
-		for text, count in tags:
-			with self.subTest():
-				self.assertContains(self.resp, text, count)
+    def test_html(self):
+        """Html must contain input tags"""
+        tags = (('<form', 1),
+                ('<input', 6),
+                ('type="text"', 3),
+                ('type="email"', 1),
+                ('type="submit"', 1))
+        for text, count in tags:
+            with self.subTest():
+                self.assertContains(self.resp, text, count)
 
-	def test_csrf(self):
-		self.assertContains(self.resp, 'csrfmiddlewaretoken')
+    def test_csrf(self):
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
 
-	def test_has_form(self):
-		"""Context must have subscription form"""
-		form = self.resp.context['form']
-		self.assertIsInstance(form, SubscriptionForm)
+    def test_has_form(self):
+        """Context must have subscription form"""
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
 
 
 class SubscribePostValid(TestCase):
-	def setUp(self):
-		data = dict(name='Tiago Almeida', cpf='12345678901', email='tyagow@hotmail.com.br', phone='48-9142-0350')
-		self.resp = self.client.post('/inscricao/', data)
+    def setUp(self):
+        data = dict(name='Tiago Almeida', cpf='12345678901', email='tyagow@hotmail.com.br', phone='48-9142-0350')
+        self.resp = self.client.post('/inscricao/', data)
 
-	def test_post(self):
-		"""Valid POST should redirect to /incricao/"""
+    def test_post(self):
+        """Valid POST should redirect to /incricao/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
-		self.assertEqual(302, self.resp.status_code)
+    def test_send_subscribe_email(self):
+        self.assertEqual(1, len(mail.outbox))
 
-	def test_send_subscribe_email(self):
-		self.assertEqual(1, len(mail.outbox))
-
-	def test_save_subscription(self):
-		self.assertTrue(Subscription.objects.exists())
+    def test_save_subscription(self):
+        self.assertTrue(Subscription.objects.exists())
 
 
 class SubscribePostInvalid(TestCase):
-	def setUp(self):
-		self.response = self.client.post('/inscricao/', {})
+    def setUp(self):
+        self.response = self.client.post('/inscricao/', {})
 
-	def test_post(self):
-		"""invalid POST Should not redirect"""
-		self.assertEqual(200, self.response.status_code)
+    def test_post(self):
+        """invalid POST Should not redirect"""
+        self.assertEqual(200, self.response.status_code)
 
-	def test_template(self):
-		self.assertTemplateUsed(self.response, 'subscriptions/subscription_form.html')
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'subscriptions/subscription_form.html')
 
-	def test_has_form(self):
-		form = self.response.context['form']
-		self.assertIsInstance(form, SubscriptionForm)
+    def test_has_form(self):
+        form = self.response.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
 
-	def test_form_has_errors(self):
-		form = self.response.context['form']
-		self.assertTrue(form.errors)
+    def test_form_has_errors(self):
+        form = self.response.context['form']
+        self.assertTrue(form.errors)
 
-	def test_dont_save_subscription(self):
-		self.assertFalse(Subscription.objects.exists())
-
-
-class SubscribeSuccessMessage(TestCase):
-	def test_message(self):
-		data = dict(name='Tiago Almeida', cpf='12345678901', email='tyagow@hotmail.com.br', phone='48-9142-0350')
-
-		response = self.client.post('/inscricao/', data, follow=True)
-		self.assertContains(response, 'Inscrição confirmada!')
+    def test_dont_save_subscription(self):
+        self.assertFalse(Subscription.objects.exists())
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import messages
 from django.core import mail
+from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
@@ -10,33 +11,44 @@ from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
-	if request.method == 'POST':
-		return create(request)
-	else:
-		return new(request)
+    if request.method == 'POST':
+        return create(request)
+    else:
+        return new(request)
 
 
 def create(request):
-	form = SubscriptionForm(request.POST)
+    form = SubscriptionForm(request.POST)
 
-	if not form.is_valid():
-		return render(request, 'subscriptions/subscription_form.html', {'form': form})
+    if not form.is_valid():
+        return render(request, 'subscriptions/subscription_form.html', {'form': form})
 
-	# Send Email
-	_send_mail('Confirmação de inscrição', settings.DEFAULT_FROM_EMAIL, form.cleaned_data['email'],
-				'subscriptions/subscription_email.txt', form.cleaned_data)
+    # Send Email
 
-	Subscription.objects.create(**form.cleaned_data)
-	# success feedback
-	messages.success(request, 'Inscrição confirmada!')
+    subscription = Subscription.objects.create(**form.cleaned_data)
 
-	return HttpResponseRedirect('/inscricao/')
+    _send_mail('Confirmação de inscrição',
+               settings.DEFAULT_FROM_EMAIL,
+               subscription.email,
+               'subscriptions/subscription_email.txt',
+               {'subscription': subscription})
+
+
+    return HttpResponseRedirect('/inscricao/{}/'.format(subscription.pk))
 
 
 def new(request):
-	return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
+    return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
+
+
+def detail(request, id):
+    try:
+        subscription = Subscription.objects.get(pk=id)
+    except Subscription.DoesNotExist:
+        raise Http404
+    return render(request, 'subscriptions/subscription_detail.html', {'subscription': subscription})
 
 
 def _send_mail(subject, from_, to, template_name, context):
-	body = render_to_string(template_name, context)
-	mail.send_mail(subject, body, from_, [from_, to])
+    body = render_to_string(template_name, context)
+    mail.send_mail(subject, body, from_, [from_, to])
