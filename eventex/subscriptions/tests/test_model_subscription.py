a1de8ed4e6a4c173e845cf8d33ed1b2cef088435
Tiago Almeida <tyagow@hotmail.com.br>
2016-09-18 09:20:46 -0300

    Adiciona campo pago

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -5,21 +5,26 @@ from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionModelTest(TestCase):
-	def setUp(self):
-		self.obj = Subscription(
-			name='Tiago Almeida',
-			cpf='00839433333',
-			email='tyagow@hotmail.com.br',
-			phone='48-91420000',
-		)
-		self.obj.save()
+    def setUp(self):
+        self.obj = Subscription(
+            name='Tiago Almeida',
+            cpf='00839433333',
+            email='tyagow@hotmail.com.br',
+            phone='48-91420000',
+        )
+        self.obj.save()
 
-	def test_create(self):
-		self.assertTrue(Subscription.objects.exists())
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
 
-	def test_created_at(self):
-		"""Subscription must have an auto created_at attr."""
-		self.assertIsInstance(self.obj.created_at, datetime)
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr."""
+        self.assertIsInstance(self.obj.created_at, datetime)
 
-	def test_str(self):
-		self.assertEqual('Tiago Almeida', str(self.obj))
+    def test_str(self):
+        self.assertEqual('Tiago Almeida', str(self.obj))
+
+    def test_paid_default_to_false(self):
+        """By default paid must be False"""
+        self.assertEqual(False, self.obj.paid)
+
