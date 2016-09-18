c3a107f2fb99f7b7497bf97d25e1ac6bb1d25b15
Tiago Almeida <tyagow@hotmail.com.br>
2016-09-18 11:00:03 -0300

    Nova action no admin criada para marcar varios clientes como pago

--- eventex/subscriptions/tests/test_form_subsription.py
+++ eventex/subscriptions/tests/test_form_subsription.py
@@ -4,10 +4,10 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 
 class SubscriptionFormTest(TestCase):
-	def setUp(self):
-		self.form = SubscriptionForm()
+    def setUp(self):
+        self.form = SubscriptionForm()
 
-	def test_form_has_fields(self):
-		"""Form must have 4 fields"""
-		expected = ['name', 'cpf', 'email', 'phone']
-		self.assertSequenceEqual(expected, list(self.form.fields))
+    def test_form_has_fields(self):
+        """Form must have 4 fields"""
+        expected = ['name', 'cpf', 'email', 'phone']
+        self.assertSequenceEqual(expected, list(self.form.fields))
