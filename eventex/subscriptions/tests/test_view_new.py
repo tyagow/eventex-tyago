e424fb7cbdbe7e0db346925e03b43046b3bb32b6
Tiago Almeida <tyagow@hotmail.com.br>
2016-09-24 15:02:45 -0300

    updating .gitignore

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -75,6 +75,7 @@ class SubscriptionsNewPostInvalid(TestCase):
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
 
+
 class TemplateRegressionTest(TestCase):
     def test_template_has_non_fields_errors(self):
         invalid_data = dict(name='Tiago Almeida', cpf='12345678901')
