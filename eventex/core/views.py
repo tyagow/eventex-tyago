056042cb87c1efd157d6b3f70d64c5a324daf64b
Tiago Almeida <tyagow@hotmail.com.br>
2016-09-29 08:31:04 -0300

    solving some front end issues

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -6,6 +6,6 @@ from django.shortcuts import render
 def home(request):
     speakers = [
         {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
-        {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'},
+        {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'}
     ]
     return render(request, 'index.html', {'speakers': speakers})
